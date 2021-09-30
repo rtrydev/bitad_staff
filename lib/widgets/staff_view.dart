daa798e574d198117b4d0d9a5db865e972c2d9e7
rtrydev <r.kulka44@gmail.com>
2021-09-30 11:49:23 +0200

    proper contact loading

--- lib/widgets/staff_view.dart
+++ lib/widgets/staff_view.dart
@@ -7,92 +7,101 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StaffView extends StatefulWidget{
+
   @override
   State<StatefulWidget> createState() => _StaffViewState();
 
 }
 
 class _StaffViewState extends State<StaffView> {
-  List<Staff>? staffList;
+  late Future<List<Staff>> staffList;
+
+  @override
+  void initState(){
+    super.initState();
+    staffList = _getStaff();
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    final api = RetrofitApi();
-    api.getApiClient().then((value) {
-      final client = RestClient(value);
-      client.getStaff().then((value) => {setState(() => staffList = value)});
-    });
-
     return Scaffold(
-      body: ListView.separated(
-        padding: EdgeInsets.only(top: 8),
-          itemBuilder: (BuildContext context, int index){
-            return Container(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          width: 70,
-                          height: 70,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: NetworkImage('${staffList?[index].picture}')
-                              )
-                          ),
-                        ),
-                        Padding(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+      body: FutureBuilder<List<Staff>>(
+        future: staffList,
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return ListView.separated(
+                padding: EdgeInsets.only(top: 8),
+                itemBuilder: (BuildContext context, int index){
+                  return Container(
+                      padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
                             children: [
-                              Text('${staffList?[index].name}', style: TextStyle(fontWeight: FontWeight.w700)),
-                              Text('${staffList?[index].description}', style: TextStyle(color: Colors.grey.shade600),),
+                              Container(
+                                width: 70,
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: NetworkImage('${snapshot.data?[index].picture}')
+                                    )
+                                ),
+                              ),
+                              Padding(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('${snapshot.data?[index].name}', style: TextStyle(fontWeight: FontWeight.w700)),
+                                    Text('${snapshot.data?[index].description}', style: TextStyle(color: Colors.grey.shade600),),
+                                  ],
+                                ),
+                                padding: EdgeInsets.only(left: 16),
+                              )
+
                             ],
                           ),
-                          padding: EdgeInsets.only(left: 16),
-                        )
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(
+                                  width: 40,
+                                  child: TextButton(
+                                    child: Icon(Icons.phone, color:Colors.blue),
+                                    onPressed: (){
+                                      _launchCaller(snapshot.data?[index].contact);
+                                    },
+                                  ) ,
+                                ),
+                                SizedBox(
+                                  width: 40,
+                                  child: TextButton(
+                                    child: Icon(Icons.message, color:Colors.blue),
+                                    onPressed: (){
+                                      _launchMessenger(snapshot.data?[index].contact);
+                                    },
+                                  ) ,
+                                )
 
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          SizedBox(
-                            width: 40,
-                            child: TextButton(
-                              child: Icon(Icons.phone, color:Colors.blue),
-                              onPressed: (){
-                                _launchCaller(staffList?[index].contact);
-                              },
-                            ) ,
-                          ),
-                          SizedBox(
-                            width: 40,
-                            child: TextButton(
-                              child: Icon(Icons.message, color:Colors.blue),
-                              onPressed: (){
-                                _launchMessenger(staffList?[index].contact);
-                              },
-                            ) ,
+                              ]
+
                           )
 
-                        ]
+                        ],
+                      )
+                  );
 
-                    )
-
-                  ],
-                )
-            );
-
-          },
-          separatorBuilder: (BuildContext context, int index) => const Divider(),
-          itemCount: staffList?.length ?? 0),
+                },
+                separatorBuilder: (BuildContext context, int index) => const Divider(),
+                itemCount: snapshot.data?.length ?? 0);
+          } else {
+            return const Center( child: CircularProgressIndicator(),) ;
+          }
+        },
+      )
     );
   }
 
@@ -113,5 +122,15 @@ class _StaffViewState extends State<StaffView> {
       throw 'Could not launch $url';
     }
   }
+
+  Future<List<Staff>> _getStaff() async {
+  final api = RetrofitApi();
+  final client = await api.getApiClient();
+  final restClient = RestClient(client);
+  return await restClient.getStaff();
+
+}
+
+
 
 }
