68a263f24a084b768085de4cc162569071877e60
rtrydev <r.kulka44@gmail.com>
2021-11-01 14:04:56 +0100

    move to production server

--- lib/api/retrofit_client.g.dart
+++ lib/api/retrofit_client.g.dart
@@ -8,7 +8,7 @@ part of 'retrofit_client.dart';
 
 class _RestClient implements RestClient {
   _RestClient(this._dio, {this.baseUrl}) {
-    baseUrl ??= 'http://212.106.184.93:8080/';
+    baseUrl ??= 'https://bitad.ath.bielsko.pl:8080/';
   }
 
   final Dio _dio;
